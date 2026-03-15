fbb1ea846e35c2f6061108a538fabf380411c8e0
Falilou-F <ffaliloufall1@gmail.com>
2026-03-15 11:58:27 +0100

    Add files via upload

---
+++ Portfolio/includes/header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>El Hadji Falilou Fall | Portfolio BTS SIO  </title>
+  <link rel="stylesheet" href="css/style.css" />
+    <aside class="sidebar">
+  <div class="profil-header">
+    <h2>El Hadji Falilou <br></h2>
+    <p>Etudiant en BTS SIO - Option SLAM</p>
+  </div>
+  <nav>
+    <ul>
+      <li><a href="#">Accueil</a></li>
+      <li><a href="#">Profil</a></li>
+      <li><a href="#">Parcours</a></li>
+      <li><a href="#">BTS SIO</a></li>
+      <li><a href="#">Compétences</a></li>
+      <li><a href="#">Certifications</a></li>
+      <li><a href="#">Projets</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+</aside>
+
+
+</head>
